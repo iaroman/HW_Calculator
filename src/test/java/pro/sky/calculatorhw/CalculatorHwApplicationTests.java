3ce85db533ffff0a421d31fe6d7fe99f42ca0945
Роман <roma_yakushin@mail.ru>
2023-05-16 15:08:41 +0300

    Install

---
+++ src/test/java/pro/sky/calculatorhw/CalculatorHwApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
